338d95e72f21d6c5eb4d8810fd37ffc310030f0e
RudenokArt <RudenokArt@yandex.ru>
2023-05-27 12:32:57 +0300

    save

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>">
 	<title>Document</title>
 </head>
 <body>
 
-	hello-word
+	<div class="header">
+		hello-word
+	</div>
 	
 </body>
 </html>
